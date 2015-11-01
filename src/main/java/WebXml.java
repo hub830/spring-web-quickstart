0128d3e1ff38823925f19d05df7ec55e95690546
hu <hub830@126.com>
2015-11-01 10:57:47 +0800

    init

---
+++ src/main/java/WebXml.java
@@ -0,0 +1,15 @@
+
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+import sample.web.ui.SampleWebUiApplication;
+
+public class WebXml extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SampleWebUiApplication.class);
+	}
+
+}

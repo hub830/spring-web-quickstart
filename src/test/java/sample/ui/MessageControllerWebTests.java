85dcbc79c61487cd33a3e777001a3112952f68a6
hub830 <hub830@126.com>
2015-11-01 19:28:03 +0800

    spring data jpa 配置

--- src/test/java/sample/ui/MessageControllerWebTests.java
+++ src/test/java/sample/ui/MessageControllerWebTests.java
@@ -16,6 +16,13 @@
 
 package sample.ui;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.util.regex.Pattern;
 
 import org.hamcrest.Description;
@@ -23,9 +30,8 @@ import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import sample.web.ui.SampleWebUiApplication;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationContextLoader;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -33,12 +39,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import sample.web.ui.SampleWebUiApplication;
 
 /**
  * A Basic Spring MVC Test for the Sample Controller"
@@ -48,7 +49,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration(classes = SampleWebUiApplication.class)
+//@ContextConfiguration(classes = SampleWebUiApplication.class)
+@ContextConfiguration(classes = SampleWebUiApplication.class, loader = SpringApplicationContextLoader.class)
 public class MessageControllerWebTests {
 
 	@Autowired
